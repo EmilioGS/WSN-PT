edb73a572e86e16e6e8f23d769735daba60b8daf
EmilioGS <emiliogallegoss2017@gmail.com>
2024-05-29 20:54:45 -0600

    Split style sheets and modify the registration form

--- appWeb/pt-app/src/controllers/index.controllers.js
+++ appWeb/pt-app/src/controllers/index.controllers.js
@@ -5,7 +5,7 @@ const app = express();//create an express object called app
 //Import RouterLogIn which contains the index controller and the URL /log-in
 const routerLogIn = express.Router(); 
 
-const title='MI FORMULARIO';
+const title='Inicio de sesión';
 
 //Render the log-in.pug on the routerLogIn URL
 routerLogIn.get('/',(req,res)=>{//metodo y ruta
