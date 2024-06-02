9d03affb59c56ec7c48cf57d4a1d108765d5fee5
kardeni <karlabenitez851@gmail.com>
2024-06-01 22:28:28 -0600

    Restriction on AdminActions, show all Info, show name of the user that ejecute an action

--- appWeb/pt-app/src/controllers/user.controller.js
+++ appWeb/pt-app/src/controllers/user.controller.js
@@ -68,7 +68,7 @@ module.exports={
     showInfo:[isAuthenticated,(req,res)=>{
         listaNodos=[];
         const userEmail = req.session.userEmail; //obtain user email form express session
-        console.log(userEmail);
+        //console.log(userEmail);
         readUser.query('SELECT * FROM Usuario where correo=?', [userEmail], (err,resultUser)=>{
             if (err) throw err;
             userID=resultUser[0].idUsuario; //save the user id from the result
@@ -76,23 +76,31 @@ module.exports={
             if (resultUser.length > 0) {
                 userName=resultUser[0].nombre;
                 lastName=resultUser[0].apellido;
+                lastName2=resultUser[0].seg_apellido;
                 email=resultUser[0].correo;
-                if(resultUser[0].bandera_administrador==1){
+                birthday=resultUser[0].fecha_nacimiento;
+                gender=resultUser[0].genero;
+                esAdmin=resultUser[0].bandera_administrador;
+                if(esAdmin==1){
                     isAdmin="Administrador";
                 }else
                     isAdmin="No es administrador";
                 psw=resultUser[0].contrasenia;
                 readUser.query('SELECT * FROM Usuario_tiene_Nodo WHERE Usuario_idUsuario=? ', [userID], (err,resultNodosAsignados)=>{
                     if (err) throw err;
-                    console.log(resultNodosAsignados);
+                    //console.log(resultNodosAsignados);
                     listaNodos = resultNodosAsignados.map(nodo => ({ nodo : nodo.Nodo_idNodo }));
-                    console.log(listaNodos);
+                    //console.log(listaNodos);
                     res.render('userInfoView',{
                         userName : userName,
                         lastName:lastName,
+                        lastName2:lastName2,
                         email:email,
+                        birthday:birthday,
+                        gender:gender,
                         isAdmin:isAdmin,
-                        listaNodos:listaNodos
+                        listaNodos:listaNodos,
+                        esAdmin:esAdmin
                     })//pug:js
                 });
             }else{
@@ -116,7 +124,9 @@ function isAuthenticated(req, res, next) {
 function showDashboard(res, result){
     userName=result[0].nombre; 
     userID = result[0].idUsuario;
-    //console.log(`Bienvenido usuario ${userName}`);
+    esAdmin=result[0].bandera_administrador;
+    nodo=result[0].nodo; //este nodo estara dado por la seleccion del usuario
+    //console.log(`Bienvenido usuario ${userName} y ${userID}`);
     const medicionesList = [];//create an empty array
     const suminsitrosList = [];//create an empty array
     readUser.query('SELECT m.* FROM Medicion m JOIN Usuario_tiene_Nodo utn ON m.idNodo = utn.Nodo_idNodo JOIN Usuario u ON utn.Usuario_idUsuario = u.idUsuario WHERE u.idUsuario = ?;', [userID], (req, resultMedicion) => {
@@ -135,27 +145,33 @@ function showDashboard(res, result){
                 k: resultMedicion[i].fosforo,
                 idNodo: resultMedicion[i].idNodo
             };
-        }
-        readUser.query('SELECT s.* FROM Suministro s JOIN Usuario u ON s.idUsuario_ejecutor = u.idUsuario WHERE u.idUsuario = ?;', [userID], (req, resultSuministro) => {
+        } //console.log(medicionesList);
+        //readUser.query('SELECT s.* FROM Suministro s JOIN Usuario u ON s.idUsuario_ejecutor = u.idUsuario WHERE u.idUsuario = ?;'
+        
+        readUser.query('SELECT s.*, u.nombre FROM Suministro s JOIN Nodo n ON s.idNodo = n.idNodo JOIN Usuario u ON s.idUsuario_ejecutor = u.idUsuario WHERE n.idNodo = ?;', [nodo], (req, resultSuministro) => {
+            //console.log(resultSuministro);
             for (let i = 0; i < resultSuministro.length; i++) {
                 if(resultSuministro[i].bandera_tipo_suministro==0){
                     tipoSuministro=="Riego";
                 }else
                 tipoSuministro="Inyeccion de nutrientes";
                 //One suministro => one element of the array (i)
-                suminsitrosList[i] = {
-                    // var in js array : var in result from DB query
-                    idSuministro: resultSuministro[i].idSuministro,
-                    hora: resultSuministro[i].hora,
-                    tipo: tipoSuministro,
-                    idNodo: resultSuministro[i].idNodo
-                    //idUsuario_ejecutor: resultSuministro[i].idUsuario_ejecutor
-                };
-            } //console.log(suminsitrosList);
+                //console.log(resultSuministro[i].idSuministro);
+                    suminsitrosList[i] = {
+                        // var in js array : var in result from DB query
+                        idSuministro: resultSuministro[i].idSuministro,
+                        hora: resultSuministro[i].hora,
+                        tipo: tipoSuministro,
+                        idNodo: resultSuministro[i].idNodo,
+                        idUsuario_ejecutor: resultSuministro[i].nombre
+                    };   
+                
+            }//console.log(suminsitrosList);
             res.render('dashboardView', {
                 usuario : userName, 
                 listMediciones :medicionesList, 
-                listSuministros:suminsitrosList
+                listSuministros:suminsitrosList,
+                esAdmin : esAdmin
             });//pug:js
         });
         
