4b35108e0c246abe0942b164319b3918228808bc
EmilioGS <emiliogallegoss2017@gmail.com>
2024-06-16 14:42:05 -0600

    Adding MySQL_MariaDB_Generic to Sink Node Configuration

--- sinkNodeConfiguration/sinkNodeConfiguration.ino
+++ sinkNodeConfiguration/sinkNodeConfiguration.ino
@@ -1,5 +1,8 @@
-#include <HardwareSerial.h>
+#include <HardwareSerial.h> //
 #include <XBee.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
 
 // Inicializar HardwareSerial para la comunicación con XBee
 HardwareSerial XBeeSerial(1); // Usando UART1 del ESP32 (GPIO 16, 17)
@@ -15,11 +18,92 @@ XBeeAddress64 addr64 = XBeeAddress64(msb, lsb);
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
 
+/* Configuration required to connect to DB MySQL */
+#include "defines.h"
+#include "Credentials.h"
+
+#include <MySQL_Generic.h>
+#define USING_HOST_NAME     true
+
+#if USING_HOST_NAME
+  char server[] = "huertalia.mysql.database.azure.com"; // Azure URL
+#else
+  IPAddress server(192, 168, 2, 112);
+#endif
+
+uint16_t server_port = 3306; //Azure DB Port
+
+char default_database[] = "huertalia"; // DB Name
+char default_table[]    = "Suministro2"; // DB Table
+
+String column1   = "idSuministro";
+String column2   = "idNodo"; 
+String column3   = "bandera_tipo_suministro";
+
+String query = String("SELECT ") + column1 + ", " + column2 + ", " + column3 + " FROM " + default_database + "." + default_table;
+
+MySQL_Connection conn((Client *)&client);
+
+// Create an instance of the cursor passing in the connection
+MySQL_Query sql_query = MySQL_Query(&conn);
+
+/* Configuration for HTTP method POST*/
+const char* serverName = "https://proyecto-terminal-896bb.web.app/update-sensor";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  while (!Serial && millis() < 5000); // wait for serial port to connect
+  /*XBee configuration*/
   XBeeSerial.begin(9600, SERIAL_8N1, 16, 17); // Configurar UART1 para pines 16 (RX) y 17 (TX)
   xbee.setSerial(XBeeSerial);
   Serial.println("Setup completed. Starting communication...");
+  /* MySQL_MariaDB_Generic Configuration*/
+  MYSQL_DISPLAY1("\nStarting Basic_Select_WiFi on", BOARD_NAME);
+  MYSQL_DISPLAY(MYSQL_MARIADB_GENERIC_VERSION);
+
+  // Initialize your WiFi module
+  #if ( USING_WIFI_ESP8266_AT  || USING_WIFIESPAT_LIB ) 
+    #if ( USING_WIFI_ESP8266_AT )
+      MYSQL_DISPLAY("Using ESP8266_AT/ESP8266_AT_WebServer Library");
+    #elif ( USING_WIFIESPAT_LIB )
+      MYSQL_DISPLAY("Using WiFiEspAT Library");
+    #endif
+  
+  // initialize serial for ESP module
+  EspSerial.begin(115200);
+  // initialize ESP module
+  WiFi.init(&EspSerial);
+
+  MYSQL_DISPLAY(F("WiFi shield init done"));
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
+    MYSQL_DISPLAY(F("WiFi shield not present"));
+    // don't continue
+    while (true);
+  }
+#endif
+
+  // Begin WiFi section
+  MYSQL_DISPLAY1("Connecting to", ssid);
+
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    MYSQL_DISPLAY0(".");
+  }
+
+  // print out info about the connection:
+  MYSQL_DISPLAY1("Connected to network. My IP address is:", WiFi.localIP());
+
+  MYSQL_DISPLAY3("Connecting to SQL Server @", server, ", Port =", server_port);
+  MYSQL_DISPLAY5("User =", user, ", PW =", password, ", DB =", default_database);
 }
 
 void loop() {
@@ -105,11 +189,55 @@ void loop() {
     //nss.println(xbee.getResponse().getErrorCode());
   }
 */
+
+ /* PSOT*/
+if ((millis() - lastTime) > timerDelay) {
+    if(WiFi.status() == WL_CONNECTED){
+      WiFiClientSecure client;
+      client.setInsecure();
+      HTTPClient https;
+
+      https.begin(client, serverName);
+      https.addHeader("Content-Type", "application/json");
+
+      char cadena1[] = "{\"node\":\"2\",\"sensorTemp\":\"22.5\",\"sensorHum\":\"100.0\",\"sensorPH\":\"7.5\",\"N\":\"123\",\"P\":\"123\",\"K\":\"123\"}";
+      int httpResponseCode = https.POST(cadena1);
+
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+      } else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+
+      https.end();
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+
+/*MySQL connections*/
+  MYSQL_DISPLAY("Connecting...");
+
+  if (conn.connectNonBlocking(server, server_port, user, password) != RESULT_FAIL){
+    delay(500);
+    runQuery();
+    conn.close();// close the connection
+  }else{
+    MYSQL_DISPLAY("\nConnect failed. Trying again on next iteration.");
+  }
+
+  MYSQL_DISPLAY("\nSleeping...");
+  MYSQL_DISPLAY("================================================");
+ 
+  delay(60000);
   Serial.println("---------- END ----------");
   delay(10000);
 }
 
-void describeError(uint8_t code) {
+void describeError(uint8_t code) {//For XBee
   switch (code) {
     case 0x01:
       Serial.println("MAC ACK failure");
@@ -141,3 +269,81 @@ void describeError(uint8_t code) {
   }
 }
 
+void runQuery(){
+  row_values *row = NULL;
+  int idSuministro[100];
+  int idNodo[100];
+  int bandera_tipo_suministro[100];
+  int row_count = 0;
+
+  // Initiate the query class instance
+  MySQL_Query query_mem = MySQL_Query(&conn);
+  
+  // Execute the query
+  MYSQL_DISPLAY(query);
+
+  // KH, check if valid before fetching
+  if ( !query_mem.execute(query.c_str()) )
+  {
+    MYSQL_DISPLAY("Querying error");
+    return;
+  }
+
+  query_mem.get_columns();
+
+  // Read the row
+  do 
+  {
+    row = query_mem.get_next_row();
+    
+    if (row != NULL) 
+    {
+      // Assuming there are three columns in the result
+      idSuministro[row_count] = atoi(row->values[0]);
+      idNodo[row_count] = atoi(row->values[1]);
+      bandera_tipo_suministro[row_count] = atoi(row->values[2]);
+
+      MYSQL_DISPLAY1("idSuministro =", idSuministro[row_count]);
+      MYSQL_DISPLAY1("idNodo =", idNodo[row_count]); 
+      MYSQL_DISPLAY1("bandera_tipo_suministro =", bandera_tipo_suministro[row_count]);  
+
+      row_count++;     
+    }
+  } while (row != NULL);
+
+  
+  delay(500);
+
+  // Delete the records that were read
+  for (int i = 0; i < row_count; i++) 
+  {
+    String delete_query = String("DELETE FROM ") + default_database + "." + default_table + " WHERE idSuministro = " + String(idSuministro[i]);
+    runDelete(delete_query);
+  }
+
+  sql_query.close();
+}
+
+void runDelete(String delete_query)
+{
+  MySQL_Query query_mem = MySQL_Query(&conn);
+
+  if (conn.connected())
+  {
+    MYSQL_DISPLAY(delete_query);
+    
+    // Execute the query
+    if ( !query_mem.execute(delete_query.c_str()) )
+    {
+      MYSQL_DISPLAY("Delete error");
+    }
+    else
+    {
+      MYSQL_DISPLAY("Data Deleted.");
+    }
+  }
+  else
+  {
+    MYSQL_DISPLAY("Disconnected from Server. Can't delete.");
+  }
+}
