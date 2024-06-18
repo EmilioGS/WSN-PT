bac89139afac636cd1958584dd8368f8ffd4b7e1
EmilioGS <emiliogallegoss2017@gmail.com>
2024-06-18 00:05:58 -0600

    Node Sink Code

--- sinkNodeConfiguration/sinkNodeConfiguration.ino
+++ sinkNodeConfiguration/sinkNodeConfiguration.ino
@@ -8,15 +8,19 @@
 HardwareSerial XBeeSerial(1); // Usando UART1 del ESP32 (GPIO 16, 17)
 XBee xbee = XBee();
 
-// Definir el payload
-uint8_t payload[20];
+// Use to define the XBee payload
+uint8_t payload[5];
 
-// Dirección del XBee receptor
+/* Tx XBee */
 uint32_t msb = 0x0013a200;
 uint32_t lsb = 0x4213dbb5; // Cambia esto según tu configuración
 XBeeAddress64 addr64 = XBeeAddress64(msb, lsb);
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+/* Rx XBee */
+XBeeResponse response = XBeeResponse();
+ZBRxResponse rx = ZBRxResponse(); // create reusable response objects for responses we expect to handle 
+ModemStatusResponse msr = ModemStatusResponse();
 
 /* Configuration required to connect to DB MySQL */
 #include "defines.h"
@@ -40,7 +44,7 @@ String column1   = "idSuministro";
 String column2   = "idNodo"; 
 String column3   = "bandera_tipo_suministro";
 
-String query = String("SELECT ") + column1 + ", " + column2 + ", " + column3 + " FROM " + default_database + "." + default_table;
+//String query = String("SELECT ") + column1 + ", " + column2 + ", " + column3 + " FROM " + default_database + "." + default_table;
 
 MySQL_Connection conn((Client *)&client);
 
@@ -92,22 +96,21 @@ void setup() {
     #elif ( USING_WIFIESPAT_LIB )
       MYSQL_DISPLAY("Using WiFiEspAT Library");
     #endif
-  
-  // initialize serial for ESP module
-  EspSerial.begin(115200);
-  // initialize ESP module
-  WiFi.init(&EspSerial);
+    
+    // initialize serial for ESP module
+    EspSerial.begin(115200);
+    // initialize ESP module
+    WiFi.init(&EspSerial);
 
-  MYSQL_DISPLAY(F("WiFi shield init done"));
+    MYSQL_DISPLAY(F("WiFi shield init done"));
 
-  // check for the presence of the shield
-  if (WiFi.status() == WL_NO_SHIELD)
-  {
-    MYSQL_DISPLAY(F("WiFi shield not present"));
-    // don't continue
-    while (true);
-  }
-#endif
+    // check for the presence of the shield
+    if (WiFi.status() == WL_NO_SHIELD){
+      MYSQL_DISPLAY(F("WiFi shield not present"));
+      // don't continue
+      while (true);
+    }
+  #endif
 
   // Begin WiFi section
   MYSQL_DISPLAY1("Connecting to", ssid);
@@ -132,41 +135,6 @@ void loop() {
 
   //print_wakeup_reason();
 
-  char message[] = {'H', 'o', 'l', 'a', ' ', 'd', 'e', 's', 'd', 'e', ' ', 'E', 'S', 'P', '3', '2'};
-  
-  Serial.println(message);
-  Serial.println(sizeof(message));
-
-  for (size_t i = 0; i < sizeof(message); i++) {
-    payload[i] = message[i];
-  }
-
-  Serial.println("Sending payload via XBee...");
-  zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
-  xbee.send(zbTx);
-
-  if (xbee.readPacket(200)) {
-    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
-      xbee.getResponse().getZBTxStatusResponse(txStatus);
-
-      uint8_t deliveryStatus = txStatus.getDeliveryStatus();
-      if (deliveryStatus == SUCCESS) {
-        Serial.println("Transmission successful.");
-      } else {
-        Serial.print("Transmission failed. Error code: ");
-        Serial.println(deliveryStatus, HEX);
-        describeError(deliveryStatus);
-      }
-    } else {
-      Serial.println("Response received, but not a ZB_TX_STATUS_RESPONSE.");
-    }
-  } else if (xbee.getResponse().isError()) {
-    Serial.print("Error reading packet. Error code: ");
-    Serial.println(xbee.getResponse().getErrorCode(), HEX);
-  } else {
-    Serial.println("No response received in time.");
-  }
-/*
   Serial.println("---------- RX ----------");
 
   xbee.readPacket(5000);
@@ -181,79 +149,145 @@ void loop() {
       xbee.getResponse().getZBRxResponse(rx);
           
       if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED) {
-          // the sender got an ACK
-          flashLed(statusLed, 10, 10);
+          Serial.println("The sender got an ACK");
+          // Get Address
+          XBeeAddress64 rxAddress = rx.getRemoteAddress64();
+          int idNode = identifyNode(rxAddress);
+          Serial.print("ID from the sender node: ");
+          Serial.println(idNode);          
+
+          // Access the payload
+          int payloadLength = rx.getDataLength();
+          uint8_t* receivedData = rx.getData();
+          
+          Serial.print("Received data: ");
+          for (int i = 0; i < payloadLength; i++) {
+            Serial.print((char)receivedData[i]);
+          }
+          Serial.println();
+
+          // Crear una cadena para almacenar el payload completo
+          char payloadString[21];
+          if (payloadLength < 22) {
+            strncpy(payloadString, (char*)receivedData, payloadLength);
+            payloadString[payloadLength] = '\0';
+          } else {
+            strncpy(payloadString, (char*)receivedData, 20);
+            payloadString[22] = '\0';
+          }
+
+          // Extraer los valores de los sensores del payload
+          char temp_string[6];
+          char hum_string[6];
+          char pH_string[4];
+          char N_string[4];
+          char P_string[4];
+          char K_string[4];
+
+          strncpy(temp_string, payloadString, 4);
+          temp_string[4] = '\0';
+
+          strncpy(hum_string, payloadString + 4, 5);
+          hum_string[5] = '\0';
+
+          strncpy(pH_string, payloadString + 9, 3);
+          pH_string[3] = '\0';
+
+          strncpy(N_string, payloadString + 12, 3);
+          N_string[3] = '\0';
+
+          strncpy(P_string, payloadString + 15, 3);
+          P_string[3] = '\0';
+
+          strncpy(K_string, payloadString + 18, 3);
+          K_string[3] = '\0';
+
+          // Imprimir los valores extraídos
+          Serial.print("Temp: ");
+          Serial.print(temp_string);
+          Serial.print(", Hum: ");
+          Serial.print(hum_string);
+          Serial.print(", pH: ");
+          Serial.print(pH_string);
+          Serial.print(", N: ");
+          Serial.print(N_string);
+          Serial.print(", P: ");
+          Serial.print(P_string);
+          Serial.print(", K: ");
+          Serial.println(K_string);
+
+          // Concatenar los valores extraídos al JSON
+          char cadena1[256];
+          snprintf(cadena1, sizeof(cadena1), 
+                   "{\"node\":\"%d\",\"sensorTemp\":\"%s\",\"sensorHum\":\"%s\",\"sensorPH\":\"%s\",\"N\":\"%s\",\"P\":\"%s\",\"K\":\"%s\"}",
+                   idNode, temp_string, hum_string, pH_string, N_string, P_string, K_string);
+
+          // Imprimir el JSON formado
+          Serial.print("JSON: ");
+          Serial.println(cadena1);
+
+          /* HTTP POST method */
+          if ((millis() - lastTime) > timerDelay){
+              if(WiFi.status() == WL_CONNECTED){
+                WiFiClientSecure client;
+                client.setInsecure();
+                HTTPClient https;
+
+                https.begin(client, serverName);
+                https.addHeader("Content-Type", "application/json");
+                //char cadena1[] = "{\"node\":\"2\",\"sensorTemp\":\"22.5\",\"sensorHum\":\"100.0\",\"sensorPH\":\"7.5\",\"N\":\"123\",\"P\":\"123\",\"K\":\"123\"}";
+                int httpResponseCode = https.POST(cadena1);
+
+                if (httpResponseCode > 0) {
+                  Serial.print("HTTP Response code: ");
+                  Serial.println(httpResponseCode);
+                } else {
+                  Serial.print("Error code: ");
+                  Serial.println(httpResponseCode);
+                }
+
+                https.end();
+              } else {
+                Serial.println("WiFi Disconnected");
+              }
+              lastTime = millis();
+            }
+
+            /*MySQL connections*/
+            MYSQL_DISPLAY("Connecting...");
+
+            if (conn.connectNonBlocking(server, server_port, user, password) != RESULT_FAIL){
+              delay(500);
+              runQuery(rxAddress, idNode);
+              conn.close();// close the connection
+            }else{
+              MYSQL_DISPLAY("\nConnect failed. Trying again on next iteration.");
+            }
+
+            MYSQL_DISPLAY("\nSleeping...");
+            MYSQL_DISPLAY("================================================");
+                  
       } else {
-          // we got it (obviously) but sender didn't get an ACK
-          flashLed(errorLed, 2, 20);
+          Serial.println("This is a ZigBee Receive Packet, but sender didn't get an ACK");       
       }
-      // set dataLed PWM to value of the first byte in the data
-      analogWrite(dataLed, rx.getData(0));
     } else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE) {
       xbee.getResponse().getModemStatusResponse(msr);
       // the local XBee sends this response on certain events, like association/dissociation
       
       if (msr.getStatus() == ASSOCIATED) {
-        // yay this is great.  flash led
-        flashLed(statusLed, 10, 10);
+        Serial.println("This is a ZigBee Modem Status Frame with status 2");
       } else if (msr.getStatus() == DISASSOCIATED) {
-        // this is awful.. flash led to show our discontent
-        flashLed(errorLed, 10, 10);
+        Serial.println("This is a ZigBee Modem Status Frame with status 3");
       } else {
-        // another status
-        flashLed(statusLed, 5, 10);
+        Serial.println("This is a ZigBee Modem Status Frame with a differnt status");
       }
     } else {
-      // not something we were expecting
-      flashLed(errorLed, 1, 25);    
+      Serial.println("It's an unknown ZigBee Frame");    
     }
   } else if (xbee.getResponse().isError()) {
     //nss.print("Error reading packet.  Error code: ");  
     //nss.println(xbee.getResponse().getErrorCode());
   }
-*/
-
- /* PSOT*/
-if ((millis() - lastTime) > timerDelay) {
-    if(WiFi.status() == WL_CONNECTED){
-      WiFiClientSecure client;
-      client.setInsecure();
-      HTTPClient https;
-
-      https.begin(client, serverName);
-      https.addHeader("Content-Type", "application/json");
-
-      char cadena1[] = "{\"node\":\"2\",\"sensorTemp\":\"22.5\",\"sensorHum\":\"100.0\",\"sensorPH\":\"7.5\",\"N\":\"123\",\"P\":\"123\",\"K\":\"123\"}";
-      int httpResponseCode = https.POST(cadena1);
-
-      if (httpResponseCode > 0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-      } else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-
-      https.end();
-    } else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
-  }
-
-/*MySQL connections*/
-  MYSQL_DISPLAY("Connecting...");
-
-  if (conn.connectNonBlocking(server, server_port, user, password) != RESULT_FAIL){
-    delay(500);
-    runQuery();
-    conn.close();// close the connection
-  }else{
-    MYSQL_DISPLAY("\nConnect failed. Trying again on next iteration.");
-  }
-
-  MYSQL_DISPLAY("\nSleeping...");
-  MYSQL_DISPLAY("================================================");
   
   /*esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
@@ -264,13 +298,31 @@ if ((millis() - lastTime) > timerDelay) {
   esp_deep_sleep_start();
   Serial.println("This will never be printed");*/
   
-  delay(60000);
+  delay(5000);
   Serial.println("---------- END ----------");
-  delay(10000);
+  delay(3000);
+}
+
+int identifyNode(XBeeAddress64 address){
+  int node = 0;
+  switch(address.getLsb()){
+    case 0x4213dbb5: //Bravo
+      node = 2;
+      break;
+    case 0x4213de41: //Charlie
+      node = 3;
+      break;
+    case 0x4213c429: //Delta
+      node = 4;
+      break;
+    case 0x4213db13: //Echo
+      node = 5;
+  }
+  return node;
 }
 
 void describeError(uint8_t code) {//For XBee
-  switch (code) {
+  switch (code){
     case 0x01:
       Serial.println("MAC ACK failure");
       break;
@@ -301,7 +353,7 @@ void describeError(uint8_t code) {//For XBee
   }
 }
 
-void runQuery(){
+void runQuery(XBeeAddress64 address, int idNode){
   row_values *row = NULL;
   int idSuministro[100];
   int idNodo[100];
@@ -310,6 +362,8 @@ void runQuery(){
 
   // Initiate the query class instance
   MySQL_Query query_mem = MySQL_Query(&conn);
+
+  String query = String("SELECT ") + column1 + ", " + column2 + ", " + column3 + " FROM " + default_database + "." + default_table + " WHERE idNodo = " + idNode;
   
   // Execute the query
   MYSQL_DISPLAY(query);
@@ -324,22 +378,103 @@ void runQuery(){
   query_mem.get_columns();
 
   // Read the row
-  do 
-  {
+  do{
     row = query_mem.get_next_row();
     
-    if (row != NULL) 
-    {
+    if(row != NULL){
       // Assuming there are three columns in the result
       idSuministro[row_count] = atoi(row->values[0]);
       idNodo[row_count] = atoi(row->values[1]);
       bandera_tipo_suministro[row_count] = atoi(row->values[2]);
-
       MYSQL_DISPLAY1("idSuministro =", idSuministro[row_count]);
       MYSQL_DISPLAY1("idNodo =", idNodo[row_count]); 
-      MYSQL_DISPLAY1("bandera_tipo_suministro =", bandera_tipo_suministro[row_count]);  
+      MYSQL_DISPLAY1("bandera_tipo_suministro =", bandera_tipo_suministro[row_count]);     
 
-      row_count++;     
+      /* Send indication to node */
+      Serial.println("---------- TX Start ----------");
+      char messageW[] = {'W', 'a', 't', 'e', 'r'};
+      char messageN[] = {'N', 'u', 't', 'r', 'i'};
+
+      if(bandera_tipo_suministro[row_count] == 0){
+        Serial.println("Water Valve");
+        for (size_t i = 0; i < sizeof(messageW); i++){
+          payload[i] = messageW[i];
+        } 
+      }else if(bandera_tipo_suministro[row_count] == 1){
+        Serial.println("Nutrient Valve");
+        for (size_t i = 0; i < sizeof(messageN); i++){
+          payload[i] = messageN[i];
+        } 
+      }  
+      
+      row_count++;           
+
+      Serial.println("Sending payload via XBee...");
+      zbTx = ZBTxRequest(address, payload, sizeof(payload));
+      xbee.send(zbTx);
+
+      if (xbee.readPacket(200)) {
+        if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
+          xbee.getResponse().getZBTxStatusResponse(txStatus);
+
+          uint8_t deliveryStatus = txStatus.getDeliveryStatus();
+          if (deliveryStatus == SUCCESS) {
+            Serial.println("Transmission successful.");
+          } else {
+            Serial.print("Transmission failed. Error code: ");
+            Serial.println(deliveryStatus, HEX);
+            describeError(deliveryStatus);
+          }
+        } else {
+          Serial.println("Response received, but not a ZB_TX_STATUS_RESPONSE.");
+        }
+      } else if (xbee.getResponse().isError()) {
+        Serial.print("Error reading packet. Error code: ");
+        Serial.println(xbee.getResponse().getErrorCode(), HEX);
+      } else {
+        Serial.println("No response received in time.");
+      }
+      Serial.println("---------- TX End ----------");
+      // Wait for Valve activation
+      Serial.println("Pause");  
+      delay(6000);
+      }else{
+      Serial.println("Empty data base");    
+
+      //Send indication to Sleep
+      char messageS[] = {'S', 'l', 'e', 'e', 'p'};
+
+      for (size_t i = 0; i < sizeof(messageS); i++) {
+        payload[i] = messageS[i];
+      }    
+
+      Serial.println("---------- TX Start ----------");   
+      Serial.println("Sending payload via XBee...");
+      zbTx = ZBTxRequest(address, payload, sizeof(payload));
+      xbee.send(zbTx);
+
+      if (xbee.readPacket(200)) {
+        if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
+          xbee.getResponse().getZBTxStatusResponse(txStatus);
+
+          uint8_t deliveryStatus = txStatus.getDeliveryStatus();
+          if (deliveryStatus == SUCCESS) {
+            Serial.println("Transmission successful.");
+          } else {
+            Serial.print("Transmission failed. Error code: ");
+            Serial.println(deliveryStatus, HEX);
+            describeError(deliveryStatus);
+          }
+        } else {
+          Serial.println("Response received, but not a ZB_TX_STATUS_RESPONSE.");
+        }
+      } else if (xbee.getResponse().isError()) {
+        Serial.print("Error reading packet. Error code: ");
+        Serial.println(xbee.getResponse().getErrorCode(), HEX);
+      } else {
+        Serial.println("No response received in time.");
+      }  
+      Serial.println("---------- TX End ----------");
     }
   } while (row != NULL);
 
@@ -347,11 +482,13 @@ void runQuery(){
   delay(500);
 
   // Delete the records that were read
-  for (int i = 0; i < row_count; i++) 
-  {
+  /*for (int i = 0; i < row_count; i++){
     String delete_query = String("DELETE FROM ") + default_database + "." + default_table + " WHERE idSuministro = " + String(idSuministro[i]);
     runDelete(delete_query);
-  }
+  }*/
+
+  String delete_query = String("DELETE FROM ") + default_database + "." + default_table + " WHERE idNodo = " + idNode;
+  runDelete(delete_query);
 
   sql_query.close();
 }
