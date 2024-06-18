5e254e4bfbdc751eba126cfe09aa5bae34ac3135
EmilioGS <emiliogallegoss2017@gmail.com>
2024-06-17 19:17:52 -0600

    Adding sleep mode to sink node

--- sinkNodeConfiguration/sinkNodeConfiguration.ino
+++ sinkNodeConfiguration/sinkNodeConfiguration.ino
@@ -52,7 +52,28 @@ const char* serverName = "https://proyecto-terminal-896bb.web.app/update-sensor"
 
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
+/*
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+/*#define TIME_TO_SLEEP  8     
 
+RTC_DATA_ATTR int bootCount = 0;
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+*/
 void setup() {
   Serial.begin(115200);
   while (!Serial && millis() < 5000); // wait for serial port to connect
@@ -108,6 +129,8 @@ void setup() {
 
 void loop() {
   Serial.println("---------- START ----------");
+
+  //print_wakeup_reason();
 
   char message[] = {'H', 'o', 'l', 'a', ' ', 'd', 'e', 's', 'd', 'e', ' ', 'E', 'S', 'P', '3', '2'};
   
@@ -231,7 +254,16 @@ if ((millis() - lastTime) > timerDelay) {
 
   MYSQL_DISPLAY("\nSleeping...");
   MYSQL_DISPLAY("================================================");
- 
+  
+  /*esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
+  
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");*/
+  
   delay(60000);
   Serial.println("---------- END ----------");
   delay(10000);
